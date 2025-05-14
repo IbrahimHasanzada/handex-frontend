076de5f3bb626b0881aa4ef635c1aa5ab70cbb9a
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-05-14 20:16:36 +0400

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
+   eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_DISABLE_ESLINT === 'true',
+  },
    reactStrictMode: true,
    images: {
       domains: ['api.drafts.az'],
