0017b181f02a4d529be34f82bbc52c5aef741405
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-05-15 00:38:58 +0400

    image collection components bugs fixed

--- src/components/about/ImageCollage.tsx
+++ src/components/about/ImageCollage.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { CSSProperties } from 'react';
+
+interface ImageData {
+    src: string;
+    alt: string;
+    desktopStyles: CSSProperties;
+    mobileStyles: CSSProperties;
+}
 
 const ImageCollage = () => {
-    const [isMobile, setIsMobile] = useState(false);
+    const [isMobile, setIsMobile] = useState<boolean>(false);
 
     useEffect(() => {
         const checkIfMobile = () => {
@@ -17,27 +25,27 @@ const ImageCollage = () => {
         return () => window.removeEventListener('resize', checkIfMobile);
     }, []);
 
-    const imageArray = [
+    const imageArray: ImageData[] = [
         {
             src: "/assets/img/about1.png",
             alt: "Index team members",
             desktopStyles: {
                 position: "absolute",
-                top: "30px",
-                left: "56px",
-                width: "270px",
-                height: "140px",
+                top: 30,
+                left: 56,
+                width: 270,
+                height: 140,
                 transform: "rotate(-2deg)",
-                zIndex: '2'
+                zIndex: 2
             },
             mobileStyles: {
                 position: "absolute",
-                top: "0",
-                left: "0",
-                width: "106px",
-                height: "57px",
+                top: 0,
+                left: 0,
+                width: 106,
+                height: 57,
                 transform: "rotate(-2deg)",
-                zIndex: '2'
+                zIndex: 2
             }
         },
         {
@@ -45,21 +53,21 @@ const ImageCollage = () => {
             alt: "Handex team members",
             desktopStyles: {
                 position: "absolute",
-                left: "62px",
-                top: "150px",
-                width: "400px",
-                height: "272px",
+                left: 62,
+                top: 150,
+                width: 400,
+                height: 272,
                 transform: "rotate(-20deg)",
-                zIndex: '1'
+                zIndex: 1
             },
             mobileStyles: {
                 position: "absolute",
-                left: "0",
-                top: "60px",
-                width: "160px",
-                height: "112px",
+                left: 0,
+                top: 60,
+                width: 160,
+                height: 112,
                 transform: "rotate(-20deg)",
-                zIndex: '1'
+                zIndex: 1
             }
         },
         {
@@ -67,21 +75,21 @@ const ImageCollage = () => {
             alt: "Company presentation",
             desktopStyles: {
                 position: "absolute",
-                top: "300px",
+                top: 300,
                 left: "50%",
                 transform: "translateX(-50%) rotate(2deg)",
-                width: "212px",
-                height: "110px",
-                zIndex: '0'
+                width: 212,
+                height: 110,
+                zIndex: 0
             },
             mobileStyles: {
                 position: "absolute",
-                top: "150px",
+                top: 150,
                 left: "50%",
                 transform: "translateX(-50%) rotate(2deg)",
-                width: "82px",
-                height: "44px",
-                zIndex: '0'
+                width: 82,
+                height: 44,
+                zIndex: 0
             }
         },
         {
@@ -89,21 +97,21 @@ const ImageCollage = () => {
             alt: "Team group photo",
             desktopStyles: {
                 position: "absolute",
-                top: "8px",
-                right: "250px",
-                width: "400px",
-                height: "220px",
+                top: 8,
+                right: 250,
+                width: 400,
+                height: 220,
                 transform: "rotate(8deg)",
-                zIndex: '3'
+                zIndex: 3
             },
             mobileStyles: {
                 position: "absolute",
-                top: "0",
+                top: 0,
                 right: "5%",
-                width: "160px",
-                height: "86px",
+                width: 160,
+                height: 86,
                 transform: "rotate(8deg)",
-                zIndex: '3'
+                zIndex: 3
             }
         },
         {
@@ -111,27 +119,27 @@ const ImageCollage = () => {
             alt: "Team members",
             desktopStyles: {
                 position: "absolute",
-                top: "216px",
-                right: "12px",
-                width: "320px",
-                height: "180px",
+                top: 216,
+                right: 12,
+                width: 320,
+                height: 180,
                 transform: "rotate(1deg)",
-                zIndex: '2'
+                zIndex: 2
             },
             mobileStyles: {
                 position: "absolute",
-                top: "82px",
-                right: "0",
-                width: "125px",
-                height: "68px",
+                top: 82,
+                right: 0,
+                width: 125,
+                height: 68,
                 transform: "rotate(1deg)",
-                zIndex: '2'
+                zIndex: 2
             }
         }
     ];
 
     return (
-        <div className=" w-full bg-gray-50 min-h-[300px] md:min-h-[600px] py-8 md:py-12">
+        <div className="w-full bg-gray-50 min-h-[300px] md:min-h-[600px] py-8 md:py-12">
             <div className="max-w-6xl mx-auto px-4">
                 <div className="relative w-full h-full">
                     <div className="relative w-full h-full flex items-center justify-center">
@@ -144,8 +152,14 @@ const ImageCollage = () => {
                                 <Image
                                     src={image.src}
                                     alt={image.alt}
-                                    width={isMobile ? parseInt(image.mobileStyles.width) : parseInt(image.desktopStyles.width)}
-                                    height={isMobile ? parseInt(image.mobileStyles.height) : parseInt(image.desktopStyles.height)}
+                                    width={isMobile ?
+                                        typeof image.mobileStyles.width === 'number' ? image.mobileStyles.width : 100 :
+                                        typeof image.desktopStyles.width === 'number' ? image.desktopStyles.width : 200
+                                    }
+                                    height={isMobile ?
+                                        typeof image.mobileStyles.height === 'number' ? image.mobileStyles.height : 100 :
+                                        typeof image.desktopStyles.height === 'number' ? image.desktopStyles.height : 200
+                                    }
                                     className="w-full h-full object-cover"
                                 />
                             </div>
