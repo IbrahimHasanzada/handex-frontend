298dedc71f9f4ca5dd47ad596f85f9cdf965fee2
emilhuseynvh <emilhuseynvh@gmail.com>
2025-05-19 20:41:47 +0400

    about section completed

--- src/app/[locale]/(locale)/about/page.tsx
+++ src/app/[locale]/(locale)/about/page.tsx
@@ -1,3 +1,4 @@
+import FeaturesSlider from '@/components/about/FeaturesSlider';
 import ImageCollage from '@/components/about/ImageCollage';
 import { getAbout, getContent } from '@/service';
 import { baseUrl } from '@/utils/url';
@@ -29,14 +30,14 @@ const AboutPage = async () => {
                     <div key={index} className={`mt-30 grid md:grid-cols-2 justify-between gap-7 base:gap-33 flex-col  ${index % 2 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
                         <div className='flex-1/2 rounded-[20px]'>
                             {item?.left_side?.type === 'image' ? (
-                                <img src={item?.left_side?.url} alt='' className='object-cover h-full w-full rounded-[20px]' />
+                                <img src={item?.left_side?.url} alt='' className='object-cover h-100 w-full rounded-[20px]' />
                             ) : (
                                 <div dangerouslySetInnerHTML={{ __html: item?.left_side?.translations[0]?.value }}></div>
                             )}
                         </div>
                         <div className='flex-1/2 rounded-[20px]'>
                             {item?.right_side?.type === 'image' ? (
-                                <img src={item?.right_side?.url} alt='' className='object-cover h-full w-full rounded-[20px]' />
+                                <img src={item?.right_side?.url} alt='' className='object-cover h-100 w-full rounded-[20px]' />
                             ) : (
                                 <div dangerouslySetInnerHTML={{ __html: item?.right_side?.translations[0]?.value }}></div>
                             )}
@@ -44,7 +45,8 @@ const AboutPage = async () => {
                     </div>
                 ))}
             </div>
-            <div className='mt-30 grid gap-11 grid-cols-3'>
+            <h2 className='md:text-center text-2xl md:text-[38px] mt-25 md:mt-30 mb-7 md:mb-12 font-bold'>Handexin üstünlükləri</h2>
+            <div className=' lg:grid hidden gap-11 grid-cols-3'>
                 {features?.map((item: any, i: number) => (
                     <div style={{ marginTop: (i >= 3) ? (i * -52) : (i * 52) + 'px' }} key={i} className={`h-54 my-72 bg-white box-shadow p-6 rounded-[20px]`}>
                         <img className='mb-3 size-16' src={item?.images[0]?.url} alt={item?.title} />
@@ -52,6 +54,9 @@ const AboutPage = async () => {
                         <div className='text-[#909090] text-sm line-clamp-4' dangerouslySetInnerHTML={{ __html: item.desc }}></div>
                     </div>
                 ))}
+            </div>
+            <div className='md:my-25 lg:hidden block'>
+                <FeaturesSlider features={features} />
             </div>
         </div >
     );
