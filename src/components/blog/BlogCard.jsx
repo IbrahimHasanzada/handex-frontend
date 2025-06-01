dcdda6f45d5af7a8b982e6606d638a39fd1b1da0
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-06-01 21:55:08 +0400

    updated

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -10,8 +10,7 @@ const BlogCard = ({ item }) => {
     const locale = useLocale();
     const handleClick = () => {
         router.push(`/${locale}/blog/${item.slug}`);
-    };
-    console.log(item?.image);
+    }
     
     return (
         <div onClick={() => handleClick()} className='rounded-2xl cursor-pointer box-shadow bg-white py-5 px-3'>
