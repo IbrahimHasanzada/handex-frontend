c9feea4f012fee56962e7c2b146f8a7b185ce02d
emilhuseynvh <emilhuseynvh@gmail.com>
2025-07-17 01:42:46 +0400

    some changes added

--- src/components/service/ServiceClient.jsx
+++ src/components/service/ServiceClient.jsx
@@ -4,11 +4,14 @@ import ServiceCard from './ServiceCard';
 import { getServices } from '@/service';
 import { useTranslations } from 'next-intl';
 
-const ServiceClient = ({ service }) => {
+const ServiceClient = ({ service, locale }) => {
     const t = useTranslations('service');
 
-    let [project, setProject] = useState();
+    let [project, setProject] = useState(service.data);
+    console.log(project);
+    
     let [total, setTotal] = useState();
+
     useEffect(() => {
         setTotal(service?.totalItems);
     }, [service]);
@@ -17,9 +20,11 @@ const ServiceClient = ({ service }) => {
 
     const handlePagination = async () => {
         setLoading(true);
-        let nextPage = count + 1;
+        let nextPage = ++count;
         let extraProject = await getServices(locale, nextPage);
-        setProject(prev => [...prev, ...extraProject.data]);
+            console.log(project);
+            
+        setProject(prev => project?.length ? [...prev, ...extraProject.data] : extraProject.data);
         setCount(nextPage);
         setLoading(false);
     };
@@ -28,11 +33,11 @@ const ServiceClient = ({ service }) => {
         <div className='wrapper pt-25'>
             <h1 className='text-[38px] mt-15 text-[#141414] font-bold mb-12'>{t('title')}</h1>
             <div className='grid lg:grid-cols-2 gap-6'>
-                {service?.data?.map((item, i) => (
+                {project?.map((item, i) => (
                     <ServiceCard key={i} item={item} />
                 ))}
             </div>
-            {total > (count + 1) * 12 && (
+            {total > (count + 1) * 8 && (
                 <button onClick={() => handlePagination()} className='flex bg-handle-gray mx-auto rounded-full items-center px-6 gap-2 h-12 my-15'>
                     <p className='text-base'>{loading ? 'Loading' : 'Daha çox'}</p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
