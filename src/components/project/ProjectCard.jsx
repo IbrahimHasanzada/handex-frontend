8dc1c4de5fa484bf8a04c2c629ca96f6e2b8d1e9
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-08-11 19:39:51 +0400

    chnages added

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -10,21 +10,22 @@ const ProjectCard = ({ item }) => {
         <Link href={`/${local}/layihe/${item.slug}`} className='w-full'>
             <div className='p-6 w-4/5 box-shadow bg-white rounded-[20px] h-auto md:h-81 flex items-end relative'>
                 <div>
-                    <h2 className='mb-2 text-base  whitespace-normal max-w-[300px] md:text-[30px] font-bold'>{item.title}</h2>
+                    <h2 className='mb-2 text-base  whitespace-normal max-w-[300px] md:text-2xl font-bold'>{item.title}</h2>
                     <div
                         className='
                         description-wrapper
+                        text-[#666]
                         md:max-w-md
-                        text-[#141414] !text-xs md:!text-base w-[80%]  font-normal line-clamp-4 mb-3
+                         !text-sm md:!text-base w-[80%]  font-normal line-clamp-4 mb-3
                         [&_p]:inline
-                        [&_span]:!text-[#141414] [&_span]:md:!text-base [&_span]:!text-xs
+                        [&_span]:!text-[#666] [&_span]:md:!text-sm [&_span]:!text-xs
                         [&_a]:inline [&_a]:!text-xs  [&_a]:md:!text-base [&_a]:font-normal [&_a]:text-[#0070f3]
-                        [&_h1]:inline [&_h1]:text-[#141414] [&_h1]:md:!text-base [&_h1]:text-xs [&_h1]:font-normal [&_h1]:mb-0
-                        [&_h2]:inline [&_h2]:text-[#141414] [&_h2]:md:!text-base [&_h2]:text-xs [&_h2]:font-normal [&_h2]:mb-0
-                        [&_h3]:inline [&_h3]:text-[#141414] [&_h3]:md:!text-base [&_h3]:text-xs [&_h3]:font-normal [&_h3]:mb-0
-                        [&_h4]:inline [&_h4]:text-[#141414] [&_h4]:md:!text-base [&_h4]:text-xs [&_h4]:font-normal [&_h4]:mb-0
-                        [&_h5]:inline [&_h5]:text-[#141414] [&_h5]:md:!text-base [&_h5]:text-xs [&_h5]:font-normal [&_h5]:mb-0
-                        [&_h6]:inline [&_h6]:text-[#141414] [&_h6]:md:!text-base [&_h6]:text-xs [&_h6]:font-normal [&_h6]:mb-0'
+                        [&_h1]:inline [&_h1]:text-[#666] [&_h1]:md:!text-xs [&_h1]:text-sm [&_h1]:font-normal [&_h1]:mb-0
+                        [&_h2]:inline [&_h2]:text-[#666] [&_h2]:md:!text-sm [&_h2]:text-sm [&_h2]:font-normal [&_h2]:mb-0
+                        [&_h3]:inline [&_h3]:text-[#666] [&_h3]:md:!text-sm [&_h3]:text-sm [&_h3]:font-normal [&_h3]:mb-0
+                        [&_h4]:inline [&_h4]:text-[#666] [&_h4]:md:!text-sm [&_h4]:text-sm [&_h4]:font-normal [&_h4]:mb-0
+                        [&_h5]:inline [&_h5]:text-[#666] [&_h5]:md:!text-sm [&_h5]:text-sm [&_h5]:font-normal [&_h5]:mb-0
+                        [&_h6]:inline [&_h6]:text-[#666] [&_h6]:md:!text-sm [&_h6]:text-sm [&_h6]:font-normal [&_h6]:mb-0'
                         dangerouslySetInnerHTML={{ __html: item.description || '' }}
                     />
                     <div className='flex items-center mt-7 md:mt-11 cursor-pointer'>
