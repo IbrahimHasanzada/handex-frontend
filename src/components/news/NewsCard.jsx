c3703f2f61b35771e6e31a6c017fe7f75ba48001
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-08-23 09:19:49 +0400

    fix(newsCard): add id attribute to NewsCard link

--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -7,7 +7,7 @@ const NewsCard = ({ item }) => {
 
 
   return (
-    <Link href={`/xeberler/${item.slug}`} className='bg-white pt-5 flex flex-col justify-between py-8 px-3 rounded-2xl'>
+    <Link id={item.slug} href={`/xeberler/${item.slug}`} className='bg-white pt-5 flex flex-col justify-between py-8 px-3 rounded-2xl'>
       <div className='w-full h-60 rounded-[12px]'>
         <img
           quality={100}
