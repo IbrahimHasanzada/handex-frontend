da1ce96ec103d463ae821bae4c41e0784553255b
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-08-23 09:19:49 +0400

    fix(TestimonialsHome): update customer feedback text

--- src/components/home/TestimonialsHome.tsx
+++ src/components/home/TestimonialsHome.tsx
@@ -9,8 +9,8 @@ const TestimonialsHome = async () => {
         <div>
             <div className='w-full flex justify-center md:pb-15'>
                 <div className='max-w-126 text-center'>
-                    <p className='text-2xl md:text-4xl font-bold text-[#141414]'>Məmnun müştərilərimizin rəylərini oxuyun.</p>
-                    <p className='text-[#787878] text-sm md:text-2xl leading-8'>Onların məmnuniyyəti bizim öhdəliyimizdir</p>
+                    <p className='text-2xl md:text-4xl font-bold mb-5 text-[#141414]'>Tələbələrimizin rəylərini oxuyun.</p>
+                    <p className='text-[#787878] text-sm md:text-2xl leading-8'> Onların uğuru və məmnuniyyəti bizim əsas öhdəliyimizdir</p>
                 </div>
             </div>
             <TestimonialsAccordion index={0} start={0} data={testimonialsData} page='home' />
