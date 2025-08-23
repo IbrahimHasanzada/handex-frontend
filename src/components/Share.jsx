12ced825fb59521dd909b4e3bcfde201acbb6efd
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-08-23 09:19:47 +0400

    feat(share): add ids to social share buttons

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -13,7 +13,7 @@ const Share = () => {
         <div className='w-max rounded-[48px] flex gap-4 items-center justify-center bg-white py-2 px-6'>
             <p className='text-lg text-[#141414]'>Paylaş</p>
             <div className='flex gap-2'>
-                <WhatsappShareButton url={shareUrl}>
+                <WhatsappShareButton id='whatsaap-share' url={shareUrl}>
                     <div className='size-10 cursor-pointer rounded-full bg-[#E8E8E8] flex justify-center items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
                             <path fillRule="evenodd" clipRule="evenodd" d="M18.7041 5.76205C17.0581 4.11505 14.8691 3.20705 12.5371 3.20605C7.73014 3.20605 3.81914 7.11505 3.81814 11.9201C3.81614 13.4491 4.21714 14.9521 4.98114 16.2771L3.74414 20.7931L8.36614 19.5811C9.64514 20.2771 11.0771 20.6421 12.5331 20.6421H12.5371C17.3421 20.6421 21.2531 16.7321 21.2551 11.9271C21.2561 9.59905 20.3501 7.41005 18.7041 5.76205Z" stroke="#141414" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -25,7 +25,7 @@ const Share = () => {
                         </svg>
                     </div>
                 </WhatsappShareButton>
-                <FacebookShareButton url={shareUrl}>
+                <FacebookShareButton id='facebook-share' url={shareUrl}>
                     <div className='size-10 cursor-pointer rounded-full bg-[#E8E8E8] flex justify-center items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="20" viewBox="0 0 13 20" fill="none">
                             <path fillRule="evenodd" clipRule="evenodd" d="M8 12H10.7498C10.9792 12 11.1792 11.8439 11.2348 11.6213L11.9848 8.62127C12.0222 8.4719 11.9886 8.31365 11.8939 8.1923C11.7991 8.07094 11.6537 8 11.4998 8H8V6C8 5.44772 8.44772 5 9 5H11.5C11.7761 5 12 4.77614 12 4.5V1.5C12 1.22386 11.7761 1 11.5 1H9C6.23858 1 4 3.23858 4 6V8H1.5C1.22386 8 1 8.22386 1 8.5V11.5C1 11.7761 1.22386 12 1.5 12H4V18.5C4 18.7761 4.22386 19 4.5 19H7.5C7.77614 19 8 18.7761 8 18.5V12Z" stroke="#141414" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -33,7 +33,7 @@ const Share = () => {
                     </div>
                 </FacebookShareButton>
 
-                <LinkedinShareButton url={shareUrl}>
+                <LinkedinShareButton id='linkedin-share' url={shareUrl}>
                     <div className='size-10 cursor-pointer rounded-full bg-[#E8E8E8] flex justify-center items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
                             <path fillRule="evenodd" clipRule="evenodd" d="M8 3H17.004C19.487 3 21.5 5.013 21.5 7.496V16.505C21.5 18.987 19.487 21 17.004 21H7.996C5.513 21 3.5 18.987 3.5 16.504V7.5C3.5 5.015 5.515 3 8 3V3Z" stroke="#141414" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
@@ -43,7 +43,7 @@ const Share = () => {
                         </svg>
                     </div>
                 </LinkedinShareButton>
-                <TelegramShareButton url={shareUrl}>
+                <TelegramShareButton id='telegram-share' url={shareUrl}>
                     <div className='size-10 cursor-pointer rounded-full bg-[#E8E8E8] flex justify-center items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
                             <path d="M12.295 16.906L10.23 18.915C9.87195 19.263 9.27295 19.109 9.12795 18.631L7.75195 14.101" stroke="#141414" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
