4e78db1d84fe17e921662348f1828f4a42061fed
Emil Huseynov <emilhuseynvh@Emils-MacBook-Air.local>
2025-09-02 00:57:44 +0400

    seo bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import '../styles/globals.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import localFont from 'next/font/local';
-import { Toaster } from 'react-hot-toast';
-import Head from 'next/head';
+import "../styles/globals.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import localFont from "next/font/local";
+import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const SFPro = localFont({
-  src: '../../public/font/sf-pro/SFPRODISPLAYMEDIUM.otf',
-  variable: '--font-sf-pro',
-  display: 'swap',
+  src: "../../public/font/sf-pro/SFPRODISPLAYMEDIUM.otf",
+  variable: "--font-sf-pro",
+  display: "swap",
 });
 
 export const metadata = {
-  title: 'Handex.edu.az',
-  description: 'Handex.edu.az - Təhsil və karyera platforması',
+  title: "Handex.edu.az",
+  description: "Handex.edu.az - Təhsil və karyera platforması",
   icons: {
-    icon: [{ url: '/icon.ico' }]
-  }
+    icon: [{ url: "/icon.ico" }],
+  },
 };
 
 export default function RootLayout({
@@ -27,36 +27,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="az">
-      <Head>
-        <link rel="icon" href="/icon.ico" />
-      </Head>
-      <body className={`antialiased ${SFPro.variable}`}>
+      <head>
         {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NHLC52SM');
-            `
-          }}
-        />
-
-        {/* GTM Noscript */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NHLC52SM"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `
-          }}
-        />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NHLC52SM');
+          `}
+        </Script>
+      </head>
+      <body className={`antialiased ${SFPro.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NHLC52SM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         <Toaster position="top-center" />
-        
+
         {children}
       </body>
     </html>
