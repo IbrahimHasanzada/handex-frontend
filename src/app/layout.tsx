97a2b961f4c2aff4d7ab75e28a2ed4659af982be
Emil Huseynov <emilhuseynvh@Emils-MacBook-Air.local>
2025-09-02 01:32:48 +0400

    seo bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,20 +27,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="az">
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NHLC52SM');
-          `}
-        </Script>
-      </head>
       <body className={`antialiased ${SFPro.variable}`}>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager Script - head-ə əlavə olunur */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NHLC52SM');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager (noscript) - body-nin əvvəlində */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NHLC52SM"
