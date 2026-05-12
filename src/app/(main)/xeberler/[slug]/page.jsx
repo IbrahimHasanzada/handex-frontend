86023add5febb8a6b1cbb5ed3972a35836dec7f2
emilhuseynvh <emilhuseynvh@gmail.com>
2026-05-12 10:08:51 +0400

    new

--- src/app/(main)/xeberler/[slug]/page.jsx
+++ src/app/(main)/xeberler/[slug]/page.jsx
@@ -41,21 +41,24 @@ const page = async ({ params }) => {
 
   return (
     <Suspense>
-      <div className='wrapper pt-30'>
+      <div className='wrapper pt-30 overflow-x-hidden'>
         <div className='text-center mt-15'>
-          <h1 className='md:text-[38px] text-[24px] font-bold'>{item.title}</h1>
+          <h1 className='md:text-[38px] text-[24px] font-bold break-words'>{item.title}</h1>
           <p className='text-base my-6'>{formatDate(item.createdAt)}</p>
         </div>
-        <div className='md:w-3/5 w-full mx-auto flex flex-col items-center justify-center'>
+        <div className='md:w-3/5 w-full max-w-full mx-auto flex flex-col items-center justify-center'>
           {item.image?.url && (
             <img className='w-full mb-15 rounded-[20px] object-cover' src={item.image.url} alt={item.title} />
           )}
           {item.description && (
-            <div className='text-base [&_a]:!text-blue-600' dangerouslySetInnerHTML={{ __html: item.description }} />
+            <div
+              className='text-base w-full max-w-full break-words [&_*]:max-w-full [&_a]:!text-blue-600 [&_img]:!w-full [&_img]:!h-auto [&_img]:rounded-[20px] [&_img]:my-4 [&_table]:block [&_table]:w-full [&_table]:overflow-x-auto'
+              dangerouslySetInnerHTML={{ __html: item.description }}
+            />
           )}
         </div>
       </div>
-      <div className='w-max mx-auto mt-10'>
+      <div className='w-max max-w-full mx-auto mt-10'>
         <Share />
       </div>
     </Suspense>
